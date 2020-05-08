9b4f09a791a743568f1dbb80f8b0fa6d7be229c4
Kedzior90 <kedra.radoslaw@gmail.com>
2020-05-08 21:47:06 +0200

    wallet class - example

--- src/main/java/CantorConsole.java
+++ src/main/java/CantorConsole.java
@@ -1,3 +1,4 @@
+import model.Wallet;
 import utils.Display;
 import utils.ConfigurationLoader;
 import java.io.IOException;
@@ -9,66 +10,73 @@ public class CantorConsole {
 
     public static void main(String[] args) throws IOException {
         configurationLoader.loadProperties();
-        int value;
 
-        do
-        {
-            display.displayMainMenu();
-            System.out.print("Enter value: \n");
-            Scanner scanner = new Scanner(System.in);
-            value = scanner.nextInt();
+        Wallet wallet = new Wallet();
+        wallet.setProvideAmount();
+        wallet.setDatabaseAmount();
+        wallet.setCountAmount();
+        System.out.println(wallet.getCountAmount());
 
-            switch(value) {
-                case 1:
-                    display.displayUserRegister();
-                    break;
-                case 2:
-                    if (display.displayLogin() == true){
-                        do {
-                            display.displayUserMenu();
-                            System.out.printf("Enter value: \n");
-                            value = scanner.nextInt();
 
-                            switch (value) {
-                                case 1:
-                                    display.displayTransaction();
-                                    break;
-                                case 2:
-                                    display.displayTradeFile();
-                                    break;
-                                case 3:
-                                    display.displayUserDatabaseFile();
-                                    break;
-                                case 4:
-                                    display.displayUserListSortedById();
-                                    break;
-                                case 5:
-                                    display.displayUserListSortedByLogin();
-                                    break;
-                                case 6:
-                                    display.displayUserListSortedByName();
-                                    break;
-                                case 7:
-                                    display.displayUserListSortedBySurname();
-                                    break;
-                                case 8:
-                                    display.displayUserListSortedByEmailAddress();
-                                    break;
-                                case 9:
-                                    display.displayUserListSortedByCreationDate();
-                                    break;
-                            }
-                        } while (value!=10);
-                    }
-                    break;
-                case 3:
-                    display.displayCheckRates();
-                    break;
-                case 4:
-                    display.displayTransaction();
-                    break;
-            }
-        } while(value!=0);
+//        int value;
+//
+//        do
+//        {
+//            display.displayMainMenu();
+//            System.out.print("Enter value: \n");
+//            Scanner scanner = new Scanner(System.in);
+//            value = scanner.nextInt();
+//
+//            switch(value) {
+//                case 1:
+//                    display.displayUserRegister();
+//                    break;
+//                case 2:
+//                    if (display.displayLogin() == true){
+//                        do {
+//                            display.displayUserMenu();
+//                            System.out.printf("Enter value: \n");
+//                            value = scanner.nextInt();
+//
+//                            switch (value) {
+//                                case 1:
+//                                    display.displayTransaction();
+//                                    break;
+//                                case 2:
+//                                    display.displayTradeFile();
+//                                    break;
+//                                case 3:
+//                                    display.displayUserDatabaseFile();
+//                                    break;
+//                                case 4:
+//                                    display.displayUserListSortedById();
+//                                    break;
+//                                case 5:
+//                                    display.displayUserListSortedByLogin();
+//                                    break;
+//                                case 6:
+//                                    display.displayUserListSortedByName();
+//                                    break;
+//                                case 7:
+//                                    display.displayUserListSortedBySurname();
+//                                    break;
+//                                case 8:
+//                                    display.displayUserListSortedByEmailAddress();
+//                                    break;
+//                                case 9:
+//                                    display.displayUserListSortedByCreationDate();
+//                                    break;
+//                            }
+//                        } while (value!=10);
+//                    }
+//                    break;
+//                case 3:
+//                    display.displayCheckRates();
+//                    break;
+//                case 4:
+//                    display.displayTransaction();
+//                    break;
+//            }
+//        } while(value!=0);
     }
 }
-//todo testt
--- src/main/java/model/Wallet.java
+++ src/main/java/model/Wallet.java
@@ -1,9 +1,47 @@
 package model;
 
+import java.util.Scanner;
+
 public class Wallet {
     public String provideAmount;
     public String databaseAmount;
     public String countAmount;
 
+    public Wallet(String provideAmount, String databaseAmount, String countAmount) {
+        this.provideAmount = provideAmount;
+        this.databaseAmount = databaseAmount;
+        this.countAmount = countAmount;
+    }
+
+    public Wallet() {
+    }
+
+    public void setProvideAmount () {
+        System.out.println("Enter amount: ");
+        Scanner scan = new Scanner(System.in);
+        this.provideAmount = scan.nextLine();
+    }
+
+    public String getProvideAmount(){
+        return provideAmount;
+    }
+
+    public void setDatabaseAmount () {
+       //dodac pobieranie z bazy
+        this.databaseAmount = "100";
+    }
+
+    public String getDatabaseAmount(){
+        return databaseAmount;
+    }
+
+    public void setCountAmount () {
+        //dodac pobieranie z bazy
+        this.countAmount = getProvideAmount() + getDatabaseAmount();
+    }
+
+    public String getCountAmount(){
+        return countAmount;
+    }
 
 }
